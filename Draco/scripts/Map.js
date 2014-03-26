0b284b5ad8bba934119af6cb354b7d3203a9b88d
walker <raymond.walker@autodesk.com>
2014-03-25 22:09:12 -0400

    new map integration

--- Draco/scripts/Map.js
+++ Draco/scripts/Map.js
@@ -1,87 +1,125 @@
-﻿/// <reference path="jquery-1.3.2.js" />
+﻿var map = null;
+var noPins = true;
+function LoadMap(latitude, longitude) {
 
-var map = null;
-var points = [];
-var shapes = [];
-var center = null;
-
-function LoadMap(latitude, longitude, onMapLoaded) {
-    map = new VEMap('theMap');
-    options = new VEMapOptions();
-    options.EnableBirdseye = false;
-
-    // Makes the control bar less obtrusize.
-    map.SetDashboardSize(VEDashboardSize.Small);
-    
-    if (onMapLoaded != null)
-        map.onLoadMap = onMapLoaded;
-
-    if (latitude != null && longitude != null) {
-        center = new VELatLong(latitude, longitude);
+    var mapOptions = {
+        credentials: "AkBXseKhXZx9aqWYyHF5ognpGb5_Z9i1RyH1Z0Pqi6VFyRhjciANULA09DtX8Xef",
+        center: new Microsoft.Maps.Location(45.5, -122.5),
+        mapTypeId: Microsoft.Maps.MapTypeId.auto,
+        zoom: 5
     }
 
-    map.LoadMap(center, null, null, null, null, null, null, options);
-}
+    map = new Microsoft.Maps.Map(document.getElementById("theMap"), mapOptions);
 
-function LoadPin(LL, name, description) {
-    var shape = new VEShape(VEShapeType.Pushpin, LL);
-
-    //Make a nice Pushpin shape with a title and description
-    shape.SetTitle("<span class=\"pinTitle\"> " + escape(name) + "</span>");
-    if (description !== undefined) {
-        shape.SetDescription("<p class=\"pinDetails\">" + 
-        escape(description) + "</p>");
-    }
-    map.AddShape(shape);
-    points.push(LL);
-    shapes.push(shape);
-}
-
-function FindAddressOnMap(where) {
-    var numberOfResults = 20;
-    var setBestMapView = true;
-    var showResults = true;
-
-    map.Find("", where, null, null, null,
-           numberOfResults, showResults, true, true,
-           setBestMapView, callbackForLocation);
-}
-
-function callbackForLocation(layer, resultsArray, places,
-            hasMore, VEErrorMessage) {
-            
-    clearMap();
-    
-    if (places == null) 
-        return;
-
-    //Make a pushpin for each place we find
-    $.each(places, function(i, item) {
-        var description = "";
-        if (item.Description !== undefined) {
-            description = item.Description;
-        }
-        var LL = new VELatLong(item.LatLong.Latitude,
-                        item.LatLong.Longitude);
-                        
-        LoadPin(LL, item.Name, description);
+    $(window).unload(function () {
+        map.dispose();
     });
 
-    //Make sure all pushpins are visible
-    if (points.length > 1) {
-        map.SetMapView(points);
-    }
+    //Add handler for the map click event.
+    Microsoft.Maps.Events.addHandler(map, 'click', displayLatLong);
 
-    //If we've found exactly one place, that's our address.
-    if (points.length === 1) {
-        $("#Latitude").val(points[0].Latitude);
-        $("#Longitude").val(points[0].Longitude);
+    //try {
+    //    // Create the tile layer source
+    //    var tileSource = new Microsoft.Maps.TileSource({ uriConstructor: 'http://www.microsoft.com/maps/isdk/ajax/layers/lidar/{quadkey}.png' });
+
+    //    // Construct the layer using the tile source
+    //    var tilelayer = new Microsoft.Maps.TileLayer({ mercator: tileSource, opacity: .7 });
+
+    //    // Push the tile layer to the map
+    //    map.entities.push(tilelayer);
+
+    //}
+    //catch (err) {
+    //    alert('Error Message:' + err.message);
+    //}
+
+    var center = map.getCenter();
+    LoadPin(center, "CP", "Capitol Park");
+
+    // Define the pushpin location
+    //var loc = new Microsoft.Maps.Location(latitude, longitude);
+    //map.setView({ center: loc, zoom: 10 });
+
+}
+
+function LoadPin(loc, name, description) {
+    var pin = new Microsoft.Maps.Pushpin(loc, { text: name });
+
+    // Create the infobox for the pushpin
+    pinInfobox = new Microsoft.Maps.Infobox(pin.getLocation(),
+        {
+            title: name,
+            description: description,
+            visible: false,
+            offset: new Microsoft.Maps.Point(0, 15)
+        });
+
+    // Add handler for the pushpin click event.
+    Microsoft.Maps.Events.addHandler(pin, 'click', displayInfobox);
+
+    // Add a handler to function that will grey out 
+    //    other pins in the collection when a new one is added
+    Microsoft.Maps.Events.addHandler(map.entities, 'entityadded', shadePins);
+
+    // Hide the infobox when the map is moved.
+    Microsoft.Maps.Events.addHandler(map, 'viewchange', hideInfobox);
+
+    // Add the pushpin and infobox to the map
+    map.entities.push(pin);
+    map.entities.push(pinInfobox);
+
+    // Center the map on the location
+    map.setView({ center: loc, zoom: 10 });
+}
+
+function displayInfobox(e) {
+    pinInfobox.setOptions({ visible: true });
+}
+
+function hideInfobox(e) {
+    pinInfobox.setOptions({ visible: false });
+}
+
+function displayLatLong(e) {
+    addPin(e);
+
+    if (e.targetType == "map") {
+        var point = new Microsoft.Maps.Point(e.getX(), e.getY());
+        var loc = e.target.tryPixelToLocation(point);
+        document.getElementById("textBox").value = loc.latitude + ", " + loc.longitude;
     }
 }
 
-function clearMap() {
-    map.Clear();
-    points = [];
-    shapes = [];
+function addPin(e) {
+    if (e.targetType == "map") {
+        var point = new Microsoft.Maps.Point(e.getX(), e.getY());
+        var loc = e.target.tryPixelToLocation(point);
+        var pin = new Microsoft.Maps.Pushpin(loc);
+
+        map.entities.push(pin);
+    }
 }
+
+function shadePins(e) {
+
+    if (noPins) {
+
+        // If there aren't yet any pins on the map, do not grey the pin out.   
+        noPins = false;
+
+    }
+    else {
+        var pin = null;
+
+        // Loop through the collection of pushpins on the map and grey out 
+        //    all but the last one added (which is at the end of the array). 
+        var i = 0;
+        for (i = 0; i < e.collection.getLength() - 1; i++) {
+            pin = e.collection.get(i);
+            pin.setOptions({ icon: "GreyPin.png" });
+        }
+    }
+}
+
+
 
