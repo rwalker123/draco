c47a9375cc0028f5ce83b458468e65d271d01048
walker <raymond.walker@autodesk.com>
2014-03-31 17:17:36 -0400

    add a bunch of missing stuff.

---
+++ Draco/scripts/photogallery.js
@@ -0,0 +1,350 @@
+﻿function InitPhotoGalleryViewModel(accountId, isAdmin, teamId) {
+    var photoGalleryElem = document.getElementById("photoGallery");
+    if (photoGalleryElem) {
+        var photoGalleryVM = new PhotoGalleryViewModel(accountId, isAdmin, teamId);
+        photoGalleryVM.init();
+        ko.applyBindings(photoGalleryVM, photoGalleryElem);
+    }
+}
+
+var PhotoGalleryViewModel = function (accountId, isAdmin, teamId) {
+    var self = this;
+
+    self.selectedFileNameDefaultText = "Select a file or drag/drop an image here.";
+
+    self.accountId = accountId;
+    self.teamId = teamId;
+    self.isAdmin = isAdmin;
+    self.viewMode = ko.observable(true);
+    self.selectedFileName = ko.observable(self.selectedFileNameDefaultText);
+    self.photoGalleryTitle = ko.observable("");
+    self.photoGalleryCaption = ko.observable("");
+    self.photoGalleryItemsCount = ko.observable();
+    self.selectedPhotoAlbum = ko.observable();
+    self.photoGalleryItems = ko.observableArray();
+    self.photoAlbums = ko.observableArray();
+    self.newPhotoAlbumName = ko.observable();
+
+    self.fileUploaderUrl = ko.computed(function () {
+        var url = window.config.rootUri + '/api/FileUploaderAPI/' + self.accountId;
+
+        if (self.teamId)
+            url = url + '/team/' + self.teamId;
+
+        url = url + '/PhotoGallery';
+
+        return url;
+    }, self);
+
+    self.init = function () {
+        if (self.isAdmin && !self.teamId)
+            self.loadPhotoAlbums();
+
+        self.loadPhotos();
+        self.initFileUpload();
+    }
+
+    self.deletePhotoAlbum = function () {
+
+        // can't delete team photo album.
+        if (self.teamId)
+            return;
+
+        $.ajax({
+            type: "DELETE",
+            url: window.config.rootUri + '/api/PhotoGalleryAPI/' + self.accountId + '/albums/' + self.selectedPhotoAlbum(),
+            success: function (id) {
+                self.photoAlbums.remove(function (item) {
+                    return item.id == id;
+                });
+                $('#photoAlbumSelect').selectpicker('refresh');
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert("Caught error: Status: " + xhr.status + ". Error: " + thrownError + "\n. responseText: " + xhr.responseText);
+            }
+        });
+    }
+
+    self.addPhotoAlbum = function () {
+
+        // teams only have a single default photo album.
+        if (self.teamId)
+            return;
+
+        $.ajax({
+            type: "POST",
+            url: window.config.rootUri + '/api/PhotoGalleryAPI/' + self.accountId + '/albums',
+            data: {
+                Id: self.newPhotoAlbumName()
+            },
+            success: function (photoAlbum) {
+                var album = {
+                    name: photoAlbum.Title,
+                    id: photoAlbum.Id
+                };
+
+                self.photoAlbums.push(album);
+
+                self.selectedPhotoAlbum(photoAlbum.Id);
+                self.newPhotoAlbumName("");
+                $('#photoAlbumSelect').selectpicker('refresh');
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert("Caught error: Status: " + xhr.status + ". Error: " + thrownError + "\n. responseText: " + xhr.responseText);
+            }
+        });
+    }
+
+    self.startEditMode = function () {
+        if (self.isAdmin) {
+            if (self.viewMode())
+                self.viewMode(false);
+            else
+                self.viewMode(true);
+        }
+    }
+
+    self.getFileNameExtension = function (filename) {
+        var a = filename.split(".");
+        if (a.length === 1 || (a[0] === "" && a.length === 2)) {
+            return "";
+        }
+        return a.pop().toLowerCase();
+    }
+
+    self.editPhoto = function (photo) {
+        photo.viewMode(false);
+        $('#photoGalleryCarousel').carousel('pause');
+    }
+
+    self.savePhoto = function (photo) {
+        var url = window.config.rootUri + '/api/PhotoGalleryAPI/' + self.accountId;
+
+        if (self.teamId)
+            url = url + '/team/' + self.teamId;
+
+        url = url + '/photos/' + photo.Id;
+
+        $.ajax({
+            type: "PUT",
+            url: url,
+            data: {
+                Id: photo.Id,
+                Title: photo.photoHeading.uncommitValue(),
+                Caption: photo.photoCaption.uncommitValue(),
+                AlbumId: photo.photoAlbumId.uncommitValue()
+            },
+            success: function (photoAlbum) {
+                photo.photoHeading.commit();
+                photo.photoCaption.commit();
+                photo.photoAlbumId.commit();
+                photo.viewMode(true);
+                $('#photoGalleryCarousel').carousel('cycle');
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert("Caught error: Status: " + xhr.status + ". Error: " + thrownError + "\n. responseText: " + xhr.responseText);
+            }
+        });
+    }
+
+    self.endEditPhoto = function (photo) {
+        photo.photoHeading.reset();
+        photo.photoCaption.reset();
+        photo.photoAlbumId.reset();
+        photo.viewMode(true);
+        $('#photoGalleryCarousel').carousel('cycle');
+    }
+
+    self.deletePhoto = function (photo) {
+
+        var url = window.config.rootUri + '/api/PhotoGalleryAPI/' + self.accountId;
+        if (self.teamId)
+            url = url + '/team/' + self.teamId;
+
+        url = url + '/photos/' + photo.Id;
+
+        $.ajax({
+            type: "DELETE",
+            url:  url,
+            success: function (id) {
+
+                photo.Id = -1;
+                photo.photoHeading("[deleted]");
+                photo.photoHeading.commit();
+                photo.photoCaption("");
+                photo.photoCaption.commit();
+                photo.viewMode(true);
+                // DELETING was messing up control. For now, just
+                // mark it deleted and it will be gone the next refresh
+                // of page.
+                //self.photoGalleryItems.remove(function (item)
+                //{
+                //    return item.Id == id;
+                //});
+                //var count = self.photoGalleryItemsCount();
+                //--count;
+                //self.photoGalleryItemsCount(count);
+
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert("Caught error: Status: " + xhr.status + ". Error: " + thrownError + "\n. responseText: " + xhr.responseText);
+            }
+        });
+
+    }
+
+    self.loadPhotos = function () {
+
+        var url = window.config.rootUri + '/api/PhotoGalleryAPI/' + self.accountId;
+
+        if (self.teamId)
+            url = url + '/team/' + self.teamId;
+
+        url = url + '/photos';
+
+        $.ajax({
+            type: "GET",
+            url:  url,
+            success: function (photos) {
+                var count = 0;
+                var mappedPhotos = $.map(photos, function (photo) {
+                    ++count;
+                    return {
+                        Id: photo.Id,
+                        photoUrl: photo.PhotoURL,
+                        photoHeading: ko.protectedObservable(photo.Title),
+                        photoCaption: ko.protectedObservable(photo.Caption),
+                        photoAlbumId: ko.protectedObservable(photo.AlbumId),
+                        viewMode: ko.observable(true)
+                    };
+                });
+
+                self.photoGalleryItems(mappedPhotos);
+                self.photoGalleryItemsCount(count);
+
+                $('.photoGalleryItemAlbum').selectpicker();
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert("Caught error: Status: " + xhr.status + ". Error: " + thrownError + "\n. responseText: " + xhr.responseText);
+            }
+        });
+    }
+
+    self.initFileUpload = function () {
+        var elem = $("#photoGallerySelectedFileName");
+
+        elem.bind('dragenter', function (e) {
+            $(this).addClass('dragover');
+        });
+
+        elem.bind('dragleave drop', function (e) {
+            $(this).removeClass('dragover');
+        });
+
+        $('#photogalleryupload')
+            .fileupload({
+                dataType: 'json',
+                dropZone: elem,
+                url: self.fileUploaderUrl(),
+                add: function (e, data) {
+                    var ext = self.getFileNameExtension(data.files[0].name);
+                    var validExtension = $.inArray(ext, ['gif', 'jpeg', 'jpg', 'png', 'bmp']);
+                    if (validExtension == -1) {
+                        return false;
+                    }
+                    // if we have a selected file, button is already visible.
+                    if (self.selectedFileName() == self.selectedFileNameDefaultText ||
+                        self.selectedFileName() == '') {
+                        data.context = $('<button/>')
+                            .text('Upload')
+                            .attr('class', 'btn btn-primary')
+                            .appendTo($('#pg'))
+                            .click(function () {
+                                data.context = $('<p/>').text('Uploading...').replaceAll($(this));
+                                data.submit();
+                            });
+                    }
+
+                    self.selectedFileName(data.files[0].name);
+                },
+                fail: function (e, data) {
+                    alert("Caught error: Status: " + data.jqXHR.status + ". Error: " + data.errorThrown + "\n. responseText: " + data.jqXHR.responseText);
+                    $('#pg').html('');
+                    data.context = $('<button/>')
+                        .text('Upload')
+                        .attr('class', 'btn btn-primary')
+                        .appendTo($('#pg'))
+                        .click(function () {
+                            data.context = $('<p/>').text('Uploading...').replaceAll($(this));
+                            data.submit();
+                        });
+                },
+                done: function (e, data) {
+                    if (data.result) {
+
+                        var photo = data.result;
+
+                        self.photoGalleryItemsCount(self.photoGalleryItemsCount() + 1);
+                        self.photoGalleryItems.push({
+                            Id: photo.Id,
+                            photoUrl: photo.PhotoURL,
+                            photoHeading: ko.protectedObservable(photo.Title),
+                            photoCaption: ko.protectedObservable(photo.Caption),
+                            photoAlbumId: ko.protectedObservable(photo.AlbumId),
+                            viewMode: ko.observable(true),
+                        });
+
+                        $('.photoGalleryItemAlbum').selectpicker();
+
+                        self.selectedFileName(self.selectedFileNameDefaultText);
+                        self.photoGalleryCaption("");
+                        self.photoGalleryTitle("");
+                        $("#pg").html('');
+                    }
+                }
+            })
+        .bind('fileuploadsubmit', function (e, data) {
+            data.formData = {
+                Id: 0,
+                Title: self.photoGalleryTitle(),
+                Caption: self.photoGalleryCaption(),
+                AlbumId: self.selectedPhotoAlbum()
+            };
+        });
+    }
+
+    self.loadPhotoAlbums = function () {
+        
+        // no albums for team.
+        if (self.teamId)
+            return;
+
+        $.ajax({
+            type: "GET",
+            url: window.config.rootUri + '/api/PhotoGalleryAPI/' + self.accountId + '/albums',
+            success: function (photoAlbums) {
+                var mappedAlbums = $.map(photoAlbums, function (album) {
+                    return {
+                        name: album.Title,
+                        id: album.Id
+                    };
+                });
+
+                self.photoAlbums(mappedAlbums);
+                self.photoAlbums.unshift({
+                    name: 'default',
+                    id: 0
+                });
+
+                self.selectedPhotoAlbum(0);
+                $('#photoAlbumSelect').selectpicker();
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                if (xhr.status == 404)
+                    return;
+
+                alert("Caught error: Status: " + xhr.status + ". Error: " + thrownError + "\n. responseText: " + xhr.responseText);
+            }
+        });
+    }
+}
---
+++ Draco/scripts/wysiwyg.js
@@ -0,0 +1,98 @@
+/* !
+ * tinymce-knockout-binding v1.0.2
+ * https://github.com/michaelpapworth/tinymce-knockout-binding 
+ *
+ * Copyright 2014 Michael Papworth
+ * Released under the MIT license
+ */
+(function( $, ko ) {
+
+	ko.bindingHandlers['wysiwyg'] = {
+		'extensions': {},
+		'init': function( element, valueAccessor, allBindings, viewModel, bindingContext ) {
+			if ( !ko.isWriteableObservable( valueAccessor() ) ) {
+				throw 'valueAccessor must be writeable and observable';
+			}
+
+			// Get custom configuration object from the 'wysiwygConfig' binding, more settings here... http://www.tinymce.com/wiki.php/Configuration
+			var options = allBindings.has( 'wysiwygConfig' ) ? allBindings.get( 'wysiwygConfig' ) : {},
+
+				// Get any extensions that have been enabled for this instance.
+				ext = allBindings.has( 'wysiwygExtensions' ) ? allBindings.get( 'wysiwygExtensions' ) : [],
+
+				// Set up a minimal default configuration
+				defaults = {
+					'browser_spellcheck': $( element ).prop( 'spellcheck' ),
+					'plugins': [ 'link', 'paste' ],
+					'toolbar': 'undo redo | bold italic | bullist numlist | link',
+					'menubar': false,
+					'statusbar': false,
+					'setup': function( editor ) {
+						// Ensure the valueAccessor state to achieve a realtime responsive UI.
+						editor.on( 'change keyup nodechange', function( args ) {
+							// Update the valueAccessor
+							valueAccessor()( editor.getContent() );
+
+							for (var name in ext) {
+								ko.bindingHandlers['wysiwyg'].extensions[ ext[ name ] ]( editor, args, allBindings, bindingContext );
+							}
+						});
+					}
+				};
+
+			// Apply custom configuration over the defaults
+			defaults = $.extend( defaults, options );
+
+			// Ensure the valueAccessor's value has been applied to the underlying element, before instanciating the tinymce plugin
+			$( element ).text( valueAccessor()() ).tinymce( defaults );
+
+			// To prevent a memory leak, ensure that the underlying element's disposal destroys it's associated editor.
+			ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+				$( element ).tinymce().remove();
+			});
+		},
+		'update': function( element, valueAccessor, allBindings, viewModel, bindingContext ) {
+		    // Implement the 'value' binding
+			return ko.bindingHandlers['value'].update( element, valueAccessor, allBindings, viewModel, bindingContext );
+		}
+	};
+
+})( jQuery, ko );
+
+(function( wysiwyg ) {
+
+	wysiwyg.extensions['dirty'] = function( editor, args, allBindings, bindingContext ) {
+		// When provided with the 'wysiwygDirty' binding, notify the accessor that the editor was touched
+		if (allBindings.has( 'wysiwygDirty' )) {
+			var accessor = allBindings.get( 'wysiwygDirty' );
+			if ( !ko.isWriteableObservable( accessor ) ) {
+				throw 'wysiwygDirty must be writeable and observable';
+			}
+			accessor( editor.isDirty() );
+		} else {
+			// ...otherwise make a sensible assumption to set the $root view-model's isDirty
+			bindingContext['$root'].isDirty = editor.isDirty();
+		}
+	};
+
+})( ko.bindingHandlers['wysiwyg'] );
+
+(function( wysiwyg ) {
+
+	wysiwyg.extensions['wordcount'] = function( editor, args, allBindings, bindingContext ) {
+		var wordCountPlugin = editor.plugins['wordcount'];
+
+		// Ensure wordcount plugin is enabled and respective binding was provided
+		if (wordCountPlugin && allBindings.has( 'wysiwygWordCount' )) {
+			var accessor = allBindings.get( 'wysiwygWordCount' );
+
+			if ( !ko.isWriteableObservable( accessor ) ) {
+				throw 'wysiwygWordCount must be writeable and observable';
+			}
+
+			// Pass the plugin's counter to the observable held by the accessor
+			accessor( wordCountPlugin.getCount() );
+		}
+	};
+
+})( ko.bindingHandlers['wysiwyg'] );
