ed2a256d05fc420bf68c0b9b0eef71a8bd516a13
walker <raymond.walker@autodesk.com>
2014-05-25 22:35:23 -0400

    Update team name from team page.

---
+++ Draco/Areas/Baseball/Controllers/TeamAPIController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SportsManager.Areas.Baseball.Controllers
+{
+    public class TeamAPIController : ApiController
+    {
+        [AcceptVerbs("PUT"), HttpPut]
+        [ActionName("teamname")]
+        public HttpResponseMessage Put(long accountId, long teamSeasonId, ModelObjects.Team t)
+        {
+            var team = DataAccess.Teams.GetTeam(t.Id);
+            if (team == null)
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+
+            team.Name = t.Name ?? String.Empty;
+            if (DataAccess.Teams.ModifyTeam(team))
+                return Request.CreateResponse<ModelObjects.Team>(HttpStatusCode.OK, team);
+            else
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+        }
+
+    }
+}
---
+++ Draco/Areas/Baseball/Scripts/team.js
@@ -0,0 +1,52 @@
+﻿function initTeamViewModel(accountId, teamId, teamName, isAdmin) {
+
+    var teamElem = document.getElementById("teamInfo");
+    if (teamElem) {
+        var teamVM = new TeamViewModel(accountId, teamId, teamName, isAdmin);
+        ko.applyBindings(teamVM, teamElem);
+    }
+}
+
+
+
+
+self.TeamViewModel = function (accountId, teamId, teamName, isAdmin) {
+    var self = this;
+
+    self.accountId = accountId;
+    self.teamId = teamId;
+    self.isAdmin = isAdmin;
+
+    self.teamName = ko.observable(teamName);
+
+    self.viewMode = ko.observable(true);
+
+    var originalTeamName = '';
+
+    self.editTeamName = function () {
+        originalTeamName = self.teamName();
+        self.viewMode(false);
+    }
+
+    self.saveTeamName = function () {
+
+        var url = window.config.rootUri + '/api/TeamAPI/' + self.accountId + '/Team/' + self.teamId + '/teamname';
+        $.ajax({
+            type: "PUT",
+            data: {
+                Id: self.teamId,
+                AccountId: self.accountId,
+                Name: self.teamName()
+            },
+            url: url,
+            success: function () {
+                self.viewMode(true);
+            }
+        });
+    }
+
+    self.cancelEdit = function () {
+        self.teamName(originalTeamName);
+        self.viewMode(true);
+    }
+}
