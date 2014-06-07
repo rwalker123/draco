e77617defc33413c89750e0fc6f5a7200cc3cb4d
walker <raymond.walker@autodesk.com>
2014-06-06 22:44:08 -0400

    tweeks to managers page, fix team managers not being able to add sponsor photo.

--- Draco/scripts/sponsors.js
+++ Draco/scripts/sponsors.js
@@ -28,7 +28,13 @@ var SponsorViewModel = function (data, accountId) {
     ko.mapping.fromJS(data, self.mapping, self);
 
     self.fileUploaderUrl = ko.computed(function () {
-        return window.config.rootUri + '/api/FileUploaderAPI/' + self.accountId + '/SponsorLogo/' + self.Id();
+        var url = window.config.rootUri + '/api/FileUploaderAPI/' + self.accountId;
+        if (self.TeamId)
+            url = url + '/TeamSponsorLogo/' + self.Id();
+        else
+            url = url + '/SponsorLogo/' + self.Id();
+
+        return url;
     });
 
 
