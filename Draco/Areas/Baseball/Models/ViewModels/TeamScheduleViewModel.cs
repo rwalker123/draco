9fc172c635d3ed985f22e65974d9d04d12b365ff
walker <raymond.walker@autodesk.com>
2014-06-09 22:15:00 -0400

    show results on team schedule.

--- Draco/Areas/Baseball/Models/ViewModels/TeamScheduleViewModel.cs
+++ Draco/Areas/Baseball/Models/ViewModels/TeamScheduleViewModel.cs
@@ -21,5 +21,76 @@ namespace SportsManager.Baseball.ViewModels
 
         public Team Team { get; set; }
         public IQueryable<Game> Games { get; set; }
+
+        public string GameSummary(long gameId)
+        {
+            ModelObjects.GameRecap recap = DataAccess.GameStats.GetGameRecap(gameId, Team.Id);
+
+            return (recap != null) ? recap.Recap : String.Empty;
+        }
+
+        public string WinLoseString(long gameWinner, int gameStatus)
+        {
+            if (gameStatus == 1)
+            {
+                if (gameWinner == -1)
+                    return String.Empty;
+                else if (gameWinner == 0)
+                    return "T";
+                else if (gameWinner == Team.Id)
+                    return "W";
+                else
+                    return "L";
+            }
+            else if (gameStatus == 2)
+            {
+                return "R";
+            }
+            else if (gameStatus == 3)
+            {
+                return "P";
+            }
+            else if (gameStatus == 4)
+            {
+                return "F";
+            }
+            else if (gameStatus == 5)
+            {
+                return "DNR";
+            }
+
+            return String.Empty;
+        }
+
+        public String Score1(long gameWinner, int homeScore, int awayScore)
+        {
+            String score = String.Empty;
+
+            if (gameWinner >= 0)
+            {
+                if (homeScore > awayScore)
+                    return homeScore.ToString();
+                else
+                    return awayScore.ToString();
+            }
+
+            return score;
+        }
+
+        public String Score2(long gameWinner, int homeScore, int awayScore)
+        {
+            String score = String.Empty;
+
+            if (gameWinner >= 0)
+            {
+                if (homeScore < awayScore)
+                    return homeScore.ToString();
+                else
+                    return awayScore.ToString();
+            }
+
+            return score;
+        }
+
     }
 }
