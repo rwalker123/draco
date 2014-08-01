ee404ed69e1a352d72ec4ea47f4ccaf508cebe10
walker <raymond.walker@autodesk.com>
2014-08-01 09:18:51 -0400

    update packages

--- Draco/Areas/Baseball/Models/ViewModels/LeagueHomeViewModel.cs
+++ Draco/Areas/Baseball/Models/ViewModels/LeagueHomeViewModel.cs
@@ -11,8 +11,11 @@ namespace SportsManager.Baseball.ViewModels
             : base(c, accountId)
         {
             SeasonName = DataAccess.Seasons.GetCurrentSeasonName(AccountId);
-            FirstYear = (Account.FirstYear == 0) ? DateTime.Now.Year : Account.FirstYear;
-            YouTubeUserId = Account.YouTubeUserId; // "GoogleDevelopers";  // "lopPrnYe7Vgh6u_TYPFHmQ"; 
+            if (Account != null)
+            {
+                FirstYear = (Account.FirstYear == 0) ? DateTime.Now.Year : Account.FirstYear;
+                YouTubeUserId = Account.YouTubeUserId; // "GoogleDevelopers";  // "lopPrnYe7Vgh6u_TYPFHmQ"; 
+            }
             ShowVideos = false; // !String.IsNullOrEmpty(YouTubeUserId) || IsAdmin;
             ShowPhotoGallery = IsAdmin || DataAccess.PhotoGallery.GetPhotos(accountId).Any();
 
