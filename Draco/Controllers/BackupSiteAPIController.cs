495099b718ea8d33ef542b66fc106e824846a758
walker <raymond.walker@autodesk.com>
2014-08-05 12:33:51 -0400

    remove db backups 5 days old.

--- Draco/Controllers/BackupSiteAPIController.cs
+++ Draco/Controllers/BackupSiteAPIController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -53,6 +54,8 @@ namespace SportsManager.Controllers
 
                 String uploadsBackupFile = backupDir + "/" + newDirName + "/" + backupFileName;
                 MoveFile(backupFullName, uploadsBackupFile);
+
+                await RemoveOldBackups();
             }
 
             return Request.CreateResponse(HttpStatusCode.OK);
@@ -70,9 +73,53 @@ namespace SportsManager.Controllers
             }
         }
 
+        private async Task<bool> RemoveOldBackups()
+        {
+            // delete backups older than 5 days.
+            Uri ftpBackupDir = new Uri(ftpUri + backupDir);
+            FtpWebRequest ftpReq = (FtpWebRequest)FtpWebRequest.Create(ftpBackupDir);
+
+            ftpReq.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
+            ftpReq.Method = WebRequestMethods.Ftp.ListDirectory;
+
+            using (FtpWebResponse ftpListResponse = (FtpWebResponse)ftpReq.GetResponse())
+            {
+                // go back 5 days.
+                var deleteOlderThan = DateTime.Now.Subtract(new TimeSpan(5, 0, 0, 0));
+
+                //Console.WriteLine("List status: {0}", ftpListResponse.StatusDescription);
+                var responseStream = ftpListResponse.GetResponseStream();
+                using (var readStream = new StreamReader(responseStream, System.Text.Encoding.UTF8))
+                {
+                    // Display the data received from the server.
+                    String dirs = await readStream.ReadToEndAsync();
+
+                    string[] alldirs = dirs.Split(new char[] { '\n' });
+                    foreach(var dir in alldirs)
+                    {
+                        if (String.IsNullOrEmpty(dir))
+                            continue;
+
+                        var folderDate = dir.TrimEnd(new char[] { '\r' });
+                        //Console.WriteLine(folderDate);
+
+                        DateTime dateCreated;
+                        if (DateTime.TryParse(folderDate, out dateCreated))
+                        {
+                            // only delete if older than 5 days.
+                            if (dateCreated < deleteOlderThan)
+                                await DeleteFolder(ftpBackupDir.ToString() + "/" + folderDate);
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
         private void CreateDir(String dirName)
         {
-            Uri backupFile = new Uri(ftpUri + "/" + backupDir + "/" + dirName);
+            Uri backupFile = new Uri(ftpUri + backupDir + "/" + dirName);
             FtpWebRequest ftpReq = (FtpWebRequest)FtpWebRequest.Create(backupFile);
             ftpReq.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
             ftpReq.Method = WebRequestMethods.Ftp.MakeDirectory;
@@ -80,6 +127,63 @@ namespace SportsManager.Controllers
             {
                 //Console.WriteLine("Delete status: {0}", ftpDeleteResponse.StatusDescription);
             }
+        }
+
+        private async Task<bool> DeleteFilesInFolder(String folderPath)
+        {
+            Uri ftpBackupDir = new Uri(folderPath);
+            FtpWebRequest ftpReq = (FtpWebRequest)FtpWebRequest.Create(ftpBackupDir);
+
+            ftpReq.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
+            ftpReq.Method = WebRequestMethods.Ftp.ListDirectory;
+
+            using (FtpWebResponse ftpListResponse = (FtpWebResponse)ftpReq.GetResponse())
+            {
+                var responseStream = ftpListResponse.GetResponseStream();
+                using (var readStream = new StreamReader(responseStream, System.Text.Encoding.UTF8))
+                {
+                    // Display the data received from the server.
+                    String files = await readStream.ReadToEndAsync();
+
+                    string[] allFiles = files.Split(new char[] { '\n' });
+                    foreach (var file in allFiles)
+                    {
+                        if (String.IsNullOrEmpty(file))
+                            continue;
+
+                        DeleteFile((folderPath + "/" + file.TrimEnd(new char[] { '\r' })).Substring(ftpUri.Length + 1));
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private async Task<bool> DeleteFolder(String folderPath)
+        {
+            //Check files inside 
+            await DeleteFilesInFolder(folderPath);
+
+            Uri dir = new Uri(folderPath);
+            FtpWebRequest ftpReq = (FtpWebRequest)FtpWebRequest.Create(dir);
+            ftpReq.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
+            ftpReq.Method = WebRequestMethods.Ftp.RemoveDirectory;
+            try
+            {
+                using (FtpWebResponse ftpDeleteResponse = (FtpWebResponse)ftpReq.GetResponse())
+                {
+                    //Console.WriteLine("Delete status: {0}", ftpDeleteResponse.StatusDescription);
+                }
+            }
+            catch (WebException ex)
+            {
+                FtpWebResponse response = (FtpWebResponse)ex.Response;
+                // ok if file doesn't exist.
+                if (response.StatusCode != FtpStatusCode.ActionNotTakenFileUnavailable)
+                    throw ex;
+            }
+
+            return true;
         }
 
         private void DeleteFile(String fileName)
