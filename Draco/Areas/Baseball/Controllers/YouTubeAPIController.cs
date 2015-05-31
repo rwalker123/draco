380f21df8313f73ba2c4de4918852a42a2584215
raymondewalker <raymondewalker@outlook.com>
2015-05-31 00:41:03 -0400

    update to  v3 YouTube API

---
+++ Draco/Areas/Baseball/Controllers/YouTubeAPIController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System.Threading.Tasks;
+
+namespace SportsManager.Areas.Baseball.Controllers
+{
+    public class YouTubeAPIController : ApiController
+    {
+        [AcceptVerbs("GET"), HttpGet]
+        [ActionName("Videos")]
+        public async Task<HttpResponseMessage> GetVideos(long accountId)
+        {
+            ModelObjects.Account a = DataAccess.Accounts.GetAccount(accountId);
+            if (a != null)
+            {
+                String youTubeId = a.YouTubeUserId;
+                if (String.IsNullOrEmpty(youTubeId))
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+
+                return await LoadVideos(youTubeId);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        [AcceptVerbs("GET"), HttpGet]
+        [ActionName("Videos")]
+        public async Task<HttpResponseMessage> TeamGetVideos(long accountId, long teamSeasonId)
+        {
+            ModelObjects.Team a = DataAccess.Teams.GetTeam(teamSeasonId);
+            if (a != null)
+            {
+                String youTubeId = a.YouTubeUserId;
+                if (String.IsNullOrEmpty(youTubeId))
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+
+                return await LoadVideos(youTubeId);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        private async Task<HttpResponseMessage> LoadVideos(String youTubeId)
+        {
+            var youtubeService = GetService();
+            var channelsListRequest = youtubeService.Channels.List("contentDetails");
+            channelsListRequest.Id = youTubeId;
+
+            // Retrieve the contentDetails part of the channel resource for the authenticated user's channel.
+            var channelsListResponse = await channelsListRequest.ExecuteAsync();
+
+            if (!channelsListResponse.Items.Any())
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+
+            var videos = new List<Object>();
+
+            var channel = channelsListResponse.Items[0];
+
+            // From the API response, extract the playlist ID that identifies the list
+            // of videos uploaded to the authenticated user's channel.
+            var uploadsListId = channel.ContentDetails.RelatedPlaylists.Uploads;
+
+            var playlistItemsListRequest = youtubeService.PlaylistItems.List("snippet");
+            playlistItemsListRequest.PlaylistId = uploadsListId;
+            playlistItemsListRequest.MaxResults = 50;
+
+            // Retrieve the list of videos uploaded to the authenticated user's channel.
+            var playlistItemsListResponse = await playlistItemsListRequest.ExecuteAsync();
+
+
+            foreach (var playlistItem in playlistItemsListResponse.Items)
+            {
+                // Print information about each video.
+                Console.WriteLine("{0} ({1})", playlistItem.Snippet.Title, playlistItem.Snippet.ResourceId.VideoId);
+                videos.Add(new
+                {
+                    title = playlistItem.Snippet.Title,
+                    thumbnailUrl = playlistItem.Snippet.Thumbnails.Default.Url,
+                    playerUrl = "http://www.youtube.com/embed/" + playlistItem.Snippet.ResourceId.VideoId
+                });
+            }
+
+            return Request.CreateResponse<IEnumerable<Object>>(HttpStatusCode.OK, videos);
+        }
+
+        private YouTubeService GetService()
+        {
+            return new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyCkaw1sO8d0LLFdytoN2xph1jXv2UA9x1U",
+                ApplicationName = "ezBaseballLeague"
+            });
+        }
+
+    }
+}
