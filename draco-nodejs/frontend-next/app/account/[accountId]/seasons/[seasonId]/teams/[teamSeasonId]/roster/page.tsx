042a30b30f1d0bbcd9921371469f4d8441e9d44e
Ray Walker <raymondewalker@outlook.com>
2025-06-29 21:41:16 -0400

    migrate roster management page

--- draco-nodejs/frontend-next/app/account/[accountId]/seasons/[seasonId]/teams/[teamSeasonId]/roster/page.tsx
+++ draco-nodejs/frontend-next/app/account/[accountId]/seasons/[seasonId]/teams/[teamSeasonId]/roster/page.tsx
@@ -1,1 +1,5 @@
-export default function Page() { return <div>account/[accountId]/seasons/[seasonId]/teams/[teamSeasonId]/roster page</div>; }
+import ProtectedTeamRosterManagement from '../../../../../../../../components/ProtectedTeamRosterManagement';
+
+export default function Page() {
+  return <ProtectedTeamRosterManagement />;
+}
