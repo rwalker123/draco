b13aa06eb63a93cde072a104e12756a5d743aa1b
Ray Walker <raymondewalker@outlook.com>
2025-08-21 22:12:45 -0400

    remove unused file

--- draco-nodejs/frontend-next/components/player-classifieds/index.ts
+++ draco-nodejs/frontend-next/components/player-classifieds/index.ts
@@ -3,14 +3,5 @@
 
 export { default as ClassifiedsHeader } from './ClassifiedsHeader';
 export { default as PlayersWantedCard } from './PlayersWantedCard';
-export { default as TeamsWantedCard } from './TeamsWantedCard';
 export { default as CreateTeamsWantedDialog } from './CreateTeamsWantedDialog';
 export { default as CreatePlayersWantedDialog } from './CreatePlayersWantedDialog';
-
-// TODO: Add more components as they are created
-// export { default as ClassifiedsFilter } from './ClassifiedsFilter';
-// export { default as ClassifiedsPagination } from './ClassifiedsPagination';
-// export { default as PositionSelector } from './PositionSelector';
-// export { default as ExperienceLevelSelector } from './ExperienceLevelSelector';
-// export { default as AccessCodeInput } from './AccessCodeInput';
-// export { default as CAPTCHAWrapper } from './CAPTCHAWrapper';
