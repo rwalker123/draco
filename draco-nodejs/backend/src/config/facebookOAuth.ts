e6dec207874859e0f4e0f45433673986e5be841f
Ray Walker <raymondewalker@outlook.com>
2025-12-13 15:59:07 -0500

    Fix: Enable Instagram permissions in Facebook OAuth configuration

--- draco-nodejs/backend/src/config/facebookOAuth.ts
+++ draco-nodejs/backend/src/config/facebookOAuth.ts
@@ -7,8 +7,8 @@ export const facebookOAuthConfig = {
     'pages_show_list',
     'pages_manage_posts',
     'pages_read_engagement',
-    //'instagram_basic',
-    //'instagram_content_publish',
+    'instagram_basic',
+    'instagram_content_publish',
   ].join(','),
   resultUrlTemplate:
     process.env.FACEBOOK_RESULT_URL_TEMPLATE ??
