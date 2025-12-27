07143b479487a18727a1b40d2a362cfbdc52b82a
Ray Walker <raymondewalker@outlook.com>
2025-12-26 21:56:01 -0500

    refactor: remove unused useDebounce callback implementation for code cleanup

--- draco-nodejs/frontend-next/hooks/useDebounce.ts
+++ draco-nodejs/frontend-next/hooks/useDebounce.ts
@@ -1,43 +1,4 @@
-import React, { useCallback, useRef, useEffect } from 'react';
-
-/**
- * Hook that provides a debounced version of a callback function
- * @param callback - Function to debounce
- * @param delay - Delay in milliseconds before executing the callback
- * @returns Debounced version of the callback
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function useDebounce<T extends (...args: any[]) => any>(callback: T, delay: number): T {
-  const callbackRef = useRef(callback);
-  const timeoutRef = useRef<NodeJS.Timeout | undefined>(undefined);
-
-  // Update callback ref when callback changes
-  useEffect(() => {
-    callbackRef.current = callback;
-  }, [callback]);
-
-  // Cleanup timeout on unmount
-  useEffect(() => {
-    return () => {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-      }
-    };
-  }, []);
-
-  return useCallback(
-    (...args: Parameters<T>) => {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-      }
-
-      timeoutRef.current = setTimeout(() => {
-        callbackRef.current(...args);
-      }, delay);
-    },
-    [delay],
-  ) as T;
-}
+import React from 'react';
 
 /**
  * Hook that provides a debounced version of a value
