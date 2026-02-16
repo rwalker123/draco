7339c3806270bf9eccbeddbb4ff241d8b44d5cfe
Ray Walker <raymondewalker@outlook.com>
2026-02-16 16:32:56 -0500

    feat: add search functionality to admin hub and implement new AdminHubSearch component

--- draco-nodejs/frontend-next/components/admin/index.ts
+++ draco-nodejs/frontend-next/components/admin/index.ts
@@ -1,3 +1,4 @@
 export { default as AdminCategoryCard, type AdminMetric } from './AdminCategoryCard';
 export { default as AdminSubItemCard, type AdminSubItemMetric } from './AdminSubItemCard';
 export { default as AdminBreadcrumbs } from './AdminBreadcrumbs';
+export { default as AdminHubSearch } from './AdminHubSearch';
